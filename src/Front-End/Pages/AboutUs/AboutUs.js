feaefd855b80fe132865e0e71db6a4bbd9f72c97
Leonardo Paredes <leonaa2@uw.edu>
2022-10-13 18:32:12 -0700

    AboutUs test filler div add

--- src/Front-End/Pages/AboutUs/AboutUs.js
+++ src/Front-End/Pages/AboutUs/AboutUs.js
@@ -8,6 +8,21 @@ function AboutUs() {
         <div className='Center'>
           <h1>This is a page about us! :)</h1>
         </div>
+
+        <div class="flex-container">
+          <div>
+            <h3>Leonardo</h3>
+            <p>Bottom Text</p>
+            {/* <img src="src\Front-End\Pages\AboutUs\AboutUsImages\Leo_doggoProfilePic.JPG" 
+              alt="Leonardo's Picture">
+            </img> */}
+          </div>
+          <div>
+            <h3>John Doe</h3>
+          </div>
+        </div>
+
+
       </div>
     );
   }
