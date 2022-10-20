3fea26d33d4c4f65fcdccebfe761aef66db2ad5d
carneyb3592 <ben.carney3@gmail.com>
2022-10-20 10:18:01 -0700

    Fixed Tests

--- src/Back-End/test/testClasses.js
+++ src/Back-End/test/testClasses.js
@@ -5,6 +5,7 @@ import {assert} from 'chai';
 describe("Testing getClassesData Function", () => {
     it('Class Data',async () => {
       let rsp = await getClassData("CSE142");
-      console.log(rsp);
+      let expected = { Difficulty: 5, Reviews: { Review1: '' } };
+      assert.deepEqual(rsp, expected);
     })
 })
