780cb66ef5ceaf04c0234a4f75bb8742fa4db7c2
Leonardo Paredes <leonaa2@uw.edu>
2022-10-23 19:04:13 -0700

    AboutUs: Added image and changed some formatting

--- src/Front-End/Pages/AboutUs/AboutUs.js
+++ src/Front-End/Pages/AboutUs/AboutUs.js
@@ -1,5 +1,7 @@
 import './AboutUs.css'
 import NavBar from '../../Components/NavBar/NavBar.js'
+import ben from '../AboutUs/AboutUsImages/benben.jpg'
+// import images from '../AboutUs/AboutUsImages'
 
 function AboutUs() {
     return (
@@ -11,32 +13,45 @@ function AboutUs() {
 
         <div class="flex-container">
           <div>
-            <h3>Leonardo</h3>
-            <p>Bottom Text</p>
-            {/* <img src="src\Front-End\Pages\AboutUs\AboutUsImages\Leo_doggoProfilePic.JPG" 
-              alt="Leonardo's Picture">
-            </img> */}
+            <h3>Ben 10</h3>
+            <p class="aboutUsPersonSubtext">
+              Junior <br></br>
+              He likes banana peppers <br></br>
+              To be changed with database
+            </p>
+            <p>He likes banana peppers</p>
+            <img src={ben} alt="ben"
+            class = "ben">
+            </img>
           </div>
+
           <div> 
             <h3>Katie Shi</h3>
             <p>Bottom Text</p>
           </div>
+
           <div>
-            <h3>John Doe</h3>
+            <h3>Tan</h3>
             <p>Bottom Text</p>
           </div>
+
           <div>
-            <h3>John Doe </h3>
+            <h3>Gary</h3>
             <p>Bottom Text</p>
           </div>
+
           <div>
-            <h3>John Doe</h3>
+            <h3>Cypress</h3>
             <p>Bottom Text</p>
           </div>
+
           <div>
-            <h3>John Doe</h3>
+            <h3>Leonardo</h3>
             <p>Bottom Text</p>
           </div>
+
+       
+
           <div>
             <h3>John Doe</h3>
             <p>Bottom Text</p>
