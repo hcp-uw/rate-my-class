91395fdb7ed4d4736add31125caeab5812ce4602
carneyb3592 <ben.carney3@gmail.com>
2022-10-31 14:52:04 -0700

    Fixed Test Cases with new database

--- src/Back-End/API/APIEndPoints.js
+++ src/Back-End/API/APIEndPoints.js
@@ -3,8 +3,8 @@ import { getData } from "../utils/utils.js";
 /* Returns data from a specified Class
    @Param Class - Specified Class Name
 */
-export async function getClassData(Class){
-    let data = await getData("Classes/" + Class);
+export async function getClassData(Test = "Classes/", Class){
+    let data = await getData(Test + Class);
     return data;
 }
 
--- src/Back-End/test/testClasses.js
+++ src/Back-End/test/testClasses.js
@@ -4,8 +4,20 @@ import {assert} from 'chai';
 
 describe("Testing getClassesData Function", () => {
     it('Class Data',async () => {
-      let rsp = await getClassData("CSE142");
-      let expected = { Difficulty: 5, Reviews: { Review1: '' } };
+      let rsp = await getClassData("Test/Classes/","CSE142");
+      let expected = {
+        Credit: 4,
+        Description: 'Test Description',
+        Difficulty: 5,
+        Reviews: 
+          { Review1: 
+            { 
+              Name: 'TestName', 
+              Rating: 1, 
+              ReviewText: 'TestText' 
+            } 
+          }
+      }
       assert.deepEqual(rsp, expected);
     })
 })
