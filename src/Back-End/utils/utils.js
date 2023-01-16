2867feab842e7d3f07d61420f65b2d3682d454dc
carneyb3592 <ben.carney3@gmail.com>
2023-01-15 16:44:23 -0800

    Changes

--- src/Back-End/utils/utils.js
+++ src/Back-End/utils/utils.js
@@ -1,4 +1,4 @@
-import { ref, get} from "firebase/database";
+import { ref, get, set} from "firebase/database";
 import { database } from "./index.js";
 
 // Returns all Data passed through the queryString
@@ -10,6 +10,13 @@ export async function getData(queryString) {
   return data;
 }
 
+
+export async function saveData(queryString,data) {
+  const query = await ref(database, queryString);
+  const qResult = await set(query,data);
+  const data = qResult.val();
+  return data;
+}
 // // Returns all Data passed through the queryString
 // // @Params queryString - String of the Query
 // export async function getSpecificData(queryString, attribute) {
