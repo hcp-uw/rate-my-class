2fc5e2dd1e2e0ac06d6297cd1a8d7a7ee6cb8a90
carneyb3592 <ben.carney3@gmail.com>
2023-04-05 15:18:35 -0700

    Added review navigation and fixed formatting

--- src/Front-End/Pages/CoursePage/CoursePage.js
+++ src/Front-End/Pages/CoursePage/CoursePage.js
@@ -1,11 +1,10 @@
 import './CoursePage.css'
 import NavBar from '../../Components/NavBar/NavBar.js'
-import grade from '../../Components/143distribution.png'
 import { Rating } from '@mui/material';
 import { useParams } from 'react-router-dom'
 import { getClassData } from '../../../Back-End/API/APIEndPoints';
 import { useEffect, useState, useRef } from 'react';
-import { CircularProgress } from '@mui/material';
+import { Pagination,CircularProgress } from '@mui/material';
 
 
 function CoursePage() {
@@ -13,54 +12,72 @@ function CoursePage() {
 
     const [classData, setClass] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [currentReviews, setCurrentReviews] = useState([]);
     const data = useRef(0);  // useRef is for preserving local value
 
     const ratingTotal = useRef(0);
     const ratingCount= useRef(0);
-
     useEffect(() => {
       const fetchData = async () => {
         data.current = await getClassData(params.classID);
-
+        ratingTotal.current = 0
         const allReviews = Object.entries(data.current.Reviews);
         ratingCount.current = allReviews.length;
         for (let i = 0; i < allReviews.length; i++) {
-          ratingTotal.current += allReviews[i][1].Rating;
+          ratingTotal.current += Number(allReviews[i][1].Rating);
         }
-
         setClass(data.current)
         setLoading(false);
+        const reviewArr = []
+        allReviews.forEach((review)=>{
+          reviewArr.push(review[1])
+        })
+        setCurrentReviews(reviewArr.slice(0,2));
+
       }
       fetchData();
     }, [params])  // second parameter to stop re-render loop
 
     // @param review: the "Reviews" table data inside course
     const individualReview = (review) => {
-      var nameLen = 60 - review.User.length;
-      var alignment = nameLen + "%";
-
       return (
         <div> 
-          <b>{review.User}</b> 
-          <Rating sx={{color: 'secondary.main', left: alignment}} name="half-rating-read" 
-                defaultValue={review.Rating} precision={1.0} readOnly />
-          <p>{review.ReviewText}</p>
+          <div className='ReviewStars'>
+            <b className='UserTitle'>{review.User}</b> 
+            <Rating sx={{color: 'secondary.main'}} 
+                      value={review.Rating} precision={1.0} readOnly />
+          </div>
+          <p className='ReviewText'>{review.ReviewText}</p>
         </div>
       )
     }
 
-    const renderReviews = () => {
-      // array access
+    const handleReviewChange = (event, value) => {
       const allReviews = Object.entries(classData.Reviews);
       const reviewArr = []
       allReviews.forEach((review)=>{
         reviewArr.push(review[1])
       })
+      const index_start = (value-1) * 2
+      const index_end = (value-1) * 2 + 1
+      setCurrentReviews(reviewArr.slice(index_start,index_end+1));
+    };
 
+    const renderReviews = () => {
+      // array access
+      const url = "/rate/" + params.classID;
+
+      const allReviews = Object.entries(classData.Reviews);
+      const reviewArr = []
+      allReviews.forEach((review)=>{
+        reviewArr.push(review[1])
+      })
       return (
+        
         <div className="Reviews">
-            <h4> Reviews </h4>
-            {reviewArr.map((obj) => individualReview(obj))}
+            <p> Reviews <span id="reviewCount">({reviewArr.length})</span><a href={url} id="RateClassButton">Rate this class</a></p>
+            {currentReviews.map((obj) => individualReview(obj))}
+            <Pagination count={Math.ceil((reviewArr.length/2))} color="secondary" onChange={handleReviewChange} />
         </div>
       )
     }
@@ -68,7 +85,7 @@ function CoursePage() {
     // class not found
     if (classData == null) {
       return (
-        <div className="SearchTest">
+        <div className="CoursePage">
           <NavBar/>
           <div className='Page'>
             <h1> "{params.classID}" doesn't exist </h1>
@@ -86,40 +103,31 @@ function CoursePage() {
     
     // display class info
     } else {
-      const url = "/rate/" + params.classID;
 
       // for some reason rendered twice, therefore divide by 2
       // console.log("total: " + ratingTotal.current);
       const averageRating = (ratingTotal.current)/(ratingCount.current) * 1.0;
       return (
-        <div className="SearchTest">
+        <div className="CoursePage">
           <NavBar/>
           <div className='Page'>
-            <header>
-              <h1> {params.classID} </h1>
-
-              <span>
-                <a href={url}>Rate this class</a>
-              </span>
-            </header>
-
-              <h2> {classData.Name} (credits: {classData.Credit})</h2>
+            <div className='Title'>
+              <p><strong>{params.classID} </strong></p>
+            </div>
+            <div className='Description'>
+            <p><strong>{classData.Name} (credits: {classData.Credit})</strong></p>
               <div className='RatingBar'>
-                <h3> Average Rating: </h3>
+              <p id='AvgRatingTxt'><strong> Average Rating: </strong></p>
                 <Rating sx={{color: 'secondary.main'}} name="half-rating-read" 
                         defaultValue={averageRating} precision={0.5} readOnly />
               </div>
-
-                <h3> Description </h3>
+              <p><strong> Description </strong></p>
                 {classData.Description}
+            </div>
 
-              <div className='GradeDistribute'>
-                  <h4> Grade Distribution </h4>
-                  <img src={grade} alt="Grade" id='143grade'/>
-              </div>
-              {renderReviews()}
-              </div>
-          </div>
+            {renderReviews()}
+            </div>
+        </div>
       );
     }
   }
