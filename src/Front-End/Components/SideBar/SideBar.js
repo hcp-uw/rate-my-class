a8061db5765f1074038dfd1b60cfd4d2cc719751
carneyb3592 <ben.carney3@gmail.com>
2023-04-14 12:59:41 -0700

    Changed sidebar to reflect login

--- src/Front-End/Components/SideBar/SideBar.js
+++ src/Front-End/Components/SideBar/SideBar.js
@@ -1,12 +1,30 @@
 import "./Sidebar.css";
 import { useNavigate } from "react-router-dom";
+import { getAuth } from 'firebase/auth'
 
 function Sidebar(props) {
     const {
         show
     } = props;
     const navigate = useNavigate();
+    const auth = getAuth()
+    const user = auth.currentUser
 
+    
+    
+    if (user) {
+        return (
+            <div className={show ? "sidebar active" : "sidebar"}>
+                <ul className="listItems">
+                    <li onClick={(e) => {navigate("/")}}>Home</li>
+                    <li onClick={(e) => {navigate("/about")}}>About Us!</li>
+                    <li onClick={(e) => {navigate("/about")}}>Feedback</li>
+                    <li><a href="/signin">Profile</a></li>
+                    <li><a href="/signin">Sign Out</a></li>
+                </ul>
+            </div>
+            )
+    }
     return (
     <div className={show ? "sidebar active" : "sidebar"}>
         <ul className="listItems">
