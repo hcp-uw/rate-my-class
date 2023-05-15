1ac9f89419a00e9326380b2989105275b4be0363
ngduytan <ngduytan@uw.edu>
2023-05-15 16:57:05 -0700

    Added and passed the test

--- src/Back-End/test/testGetSortedReview.js
+++ src/Back-End/test/testGetSortedReview.js
@@ -2,11 +2,29 @@ import {getReviewsSortedByDate} from "../API/APIEndPoints.js"
 import {assert} from 'chai';
 
 describe("Testing getReviewsSortedByDate Function", () => {
-    it('Class Data',async () => {
+    it('Review Sort',async () => {
       let reviews = await getReviewsSortedByDate("CSE142");
-      reviews.forEach((review) => {
-        console.log(`Review Text: ${review.ReviewText}`);
-        console.log(`Date: ${review.Date}`);
-      });
+      let expected = [{
+          Date: 'May 15, 2023',
+          Hash: 1211993698323931,
+          Rating: 4,
+          ReviewText: 'Test',
+          User: 'Leonardo Rudby Paredes Alfaro'
+        },
+        {
+          Date: 'May 14, 2023',
+          Hash: 4111424264421769,
+          Rating: 3,
+          ReviewText: 'This is super dope',
+          User: 'Benjamin Carney'
+        },
+        {
+          Date: 'May 10, 2023',
+          Hash: 4866666744959625,
+          Rating: 4,
+          ReviewText: 'This is another test',
+          User: 'Benjamin Carney'
+        }];
+      assert.deepEqual(reviews, expected);
     })
 })
